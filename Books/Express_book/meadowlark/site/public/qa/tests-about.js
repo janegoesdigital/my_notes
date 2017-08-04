e910a140460d48b6a03a500a5023b8e6c2bccb24
jadarling <janeodarling@gmail.com>
2017-08-04 14:52:47 +1000

    Remove book summary from git

--- Books/Express_book/meadowlark/site/public/qa/tests-about.js
+++
@@ -1,7 +0,0 @@
-//This is an example of a page test
-
-suite(' "About" Page Tests', function(){
- test('Let\'s get it right betty! page should contain link to contact page', function(){
-  assert($('a[href="/contact"]').length);
- });
-});
